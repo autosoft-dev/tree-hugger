05e6401b43ba1ab5fbdefc87739eace7cc43908b
Clement Dluzniewski <clement.dluzniewski@gmail.com>
2020-07-29 16:06:16 +0200

    implement C++ parser

--- tree_hugger/core/__init__.py
+++ tree_hugger/core/__init__.py
@@ -3,4 +3,5 @@ from .parser import PythonParser
 from .parser import PHPParser
 from .parser import JavaParser
 from .parser import JavascriptParser
+from .parser import CPPParser
 from .code_parser import match_from_span
--- tree_hugger/core/parser/__init__.py
+++ tree_hugger/core/parser/__init__.py
@@ -2,3 +2,4 @@ from .python.python_parser import PythonParser
 from .php.php_parser import PHPParser
 from .java.java_parser import JavaParser
 from .javascript.javascript_parser import JavascriptParser
+from .cpp.cpp_parser import CPPParser
