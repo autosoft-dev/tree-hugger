683150dd26adacc1dc52ab747cd9f898bb604a3d
Clement Dluzniewski <clement.dluzniewski@gmail.com>
2020-07-30 16:29:13 +0200

    add tests

--- tests/tests_python_parser.py
+++ tests/tests_python_parser.py
@@ -53,6 +53,24 @@ def test_parser_get_all_class_names(python_parser):
 		'AnotherClass'
 	])
 
+def test_parser_get_all_function_bodies(python_parser):
+	assert python_parser.get_all_function_bodies()["second_child"] == \
+		'def second_child[]:\n        """\n        This is second child\n        """\n        for i in range(10):\n            print(i)\n\n        return "Call me Liam"'
+	assert python_parser.get_all_function_bodies(get_index=True)["second_child"] == (
+		'def second_child[]:\n        """\n        This is second child\n        """\n        for i in range(10):\n            print(i)\n\n        return "Call me Liam"', 
+		(22, 8),
+		(28, 29)
+	)
+
+def test_parser_get_all_function_docstrings(python_parser):
+	assert python_parser.get_all_function_docstrings()["second_child"] == \
+		'"""\n        This is second child\n        """'
+	assert python_parser.get_all_function_docstrings(get_index=True)["second_child"] == (
+		'"""\n        This is second child\n        """', 
+		(22, 8), 
+		(28, 29)
+	)
+		
 def test_parser_get_all_function_documentations(python_parser):
 	assert python_parser.get_all_function_documentations() == {
 		'first_child': '"""\n        This is first child\n        """',
