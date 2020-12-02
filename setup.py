b00b57f87ee2651f101577012a3d07a6e21a3068
Shubhadeep Roychowdhury <shubhadeeproychowdhury@gmail.com>
2020-12-02 09:57:22 +0100

    core: fix #43

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
 ],
-version="0.10.0",
+version="0.10.1",
 packages=find_packages(exclude=("tests",)),
 install_requires=["tree-sitter", "pygit2", "pytest", "PyYAML"],
 entry_points = {
